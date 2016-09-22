e4ae7446d1dd0be04911c2e16918e10d5586b252
Joey Sherman <joey.a.sherman@gmail.com>
2016-09-22 06:43:24 -0700

    Add in selector files and styles for HomePage, Also the request helper for API calls

--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -1,3 +1,15 @@
 /**
  * Created by Joey on 9/22/2016.
  */
+
+import { fromJS } from 'immutable';
+
+const initialState = fromJS({
+
+});
+
+function homeReducer (state = initialState, action){
+
+}
+
+export default homeReducer;
--- app/containers/HomePage/sagas.js
+++ app/containers/HomePage/sagas.js
@@ -1,3 +1,6 @@
 /**
  * Created by Joey on 9/22/2016.
  */
+
+
+import { take, put, fork } from 'redux-saga';
