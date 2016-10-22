63f5834bd44a0c012189dc48cc61cd0094a9c5b7
Joey Sherman <joey.a.sherman@gmail.com>
2016-10-22 02:54:22 -0700

    Add constants for MapPage

--- app/containers/MapPage/constants.js
+++ app/containers/MapPage/constants.js
@@ -1,3 +1,13 @@
 /**
  * Created by Joey on 10/22/2016.
  */
+
+/*
+* Constants for actions
+*
+* */
+
+export const MAP_LOAD_SUCCESS = 'MAP_LOAD_SUCCESS';
+export const MAP_LOAD_ERROR = 'MAP_LOAD_ERROR';
+export const MAP_LOAD_PENDING = 'MAP_LOAD_PENDING';
+
