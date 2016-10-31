ec8786d496c6c57f89c8c9861832887ddeaddb91
Joey Sherman <joey.a.sherman@gmail.com>
2016-10-30 18:16:51 -0700

    Fix incorrect import

--- app/components/PlaceCard/index.js
+++ app/components/PlaceCard/index.js
@@ -6,15 +6,20 @@
 
 import React from 'react';
 import {Card, CardActions, CardHeader, CardTitle, CardText} from 'material-ui/Card';
-import { FlatButton } from 'material-ui/FlatButton';
+import FlatButton from 'material-ui/FlatButton';
 import styles from './styles.css';
 
 function PlaceCard(props) { // eslint-disable-line react/prefer-stateless-function
-    return (
+  return (
       <Card>
         <CardHeader
           title={props.place.name}
+          subtitle={props.place.formatted_address}
         />
+        <CardActions>
+          <FlatButton label="Expand" />
+          <FlatButton label="Reduce" />
+        </CardActions>
       </Card>
     );
 }
