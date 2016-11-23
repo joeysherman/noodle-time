4f2727e6a710c8bd21fb424f61b35c44286627bb
Joey Sherman <joey.a.sherman@gmail.com>
2016-11-23 07:22:04 -0800

    Add List component

---
+++ app/components/List/index.js
@@ -0,0 +1,75 @@
+/**
+*
+* List
+*
+*/
+
+
+import React, {Component, PropTypes} from 'react';
+import {List, ListItem, makeSelectable} from 'material-ui/List';
+import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+import Subheader from 'material-ui/Subheader';
+import styles from './styles.css';
+
+
+let SelectableList = makeSelectable(List);
+
+function wrapState(ComposedComponent) {
+  return class SelectableList extends Component {
+    static propTypes = {
+      children: PropTypes.node.isRequired,
+      defaultValue: PropTypes.number.isRequired,
+    };
+
+    componentWillMount() {
+      this.setState({
+        selectedIndex: this.props.defaultValue,
+      });
+    }
+
+    handleRequestChange = (event, index) => {
+      console.log(index);
+      this.setState({
+        selectedIndex: index,
+      });
+    };
+
+    render() {
+      return (
+        <ComposedComponent
+          value={this.state.selectedIndex}
+          onChange={this.handleRequestChange}
+          className={this.props.className}
+        >
+          {this.props.children}
+        </ComposedComponent>
+      );
+    }
+  };
+}
+
+SelectableList = wrapState(SelectableList);
+
+const renderListItems = (props, index) => (
+  <ListItem
+    key={index}
+    value={props.index}
+    primaryText={props.name}
+    leftAvatar={<Avatar src={props.image_url}/>}
+    rightAvatar={<img src={props.rating_img_url}/>}
+  />
+);
+
+const ListExampleSelectable = (props) => (
+  <Paper className={styles.listWrapper}>
+    <SelectableList defaultValue={1} className={styles.list}>
+      <Subheader>Selectable Contacts</Subheader>
+      {props.places.map((item, i) => {
+        return renderListItems(item, i+1);
+      })}
+    </SelectableList>
+  </Paper>
+);
+
+export default ListExampleSelectable;
--- app/containers/PlacesPage/index.js
+++ app/containers/PlacesPage/index.js
@@ -7,8 +7,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectPlaces } from './selectors';
+import { push } from 'react-router-redux';
 import styles from './styles.css';
 import { placesRequest } from './actions';
+import PlaceCard from '../../components/PlaceCard';
+import List from '../../components/List';
 
 import CircularProgress from 'material-ui/CircularProgress';
 
@@ -32,12 +35,24 @@ export class PlacesPage extends React.Component { // eslint-disable-line react/p
     return (lat < 90 && lat > -90 && lng < 180 && lng > -180);
   };
 
+  getMainContent = () => {
+    switch (this.props.mode) {
+      case 'list' :
+        return (
+          <List places={this.props.places.toJS()}/>
+        );
+      default :
+        return (
+          <div className={styles.ramenWrapper}><PlaceCard place={this.props.places.get(this.props.index).toJS()}/></div>
+        )
+    }
+  };
 
   render() {
 
     return (
       <div className={styles.placesPage}>
-        {this.props.places ? 'Places Found!' : <div className={styles.loadingWrapper}><CircularProgress/></div>}
+        {this.props.places ? this.getMainContent.call(this) : <div className={styles.loadingWrapper}><CircularProgress/></div>}
       </div>
     );
   }
@@ -48,12 +63,17 @@ const mapStateToProps = (state, ownProps) => {
   return {
     lat: ownProps.location.query.lat,
     lng: ownProps.location.query.lng,
+    index: ownProps.location.query.i || 0,
     places: selectPlaces(state),
+    mode: ownProps.location.query.mode,
   };
 };
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch, ownProps) {
   return {
+    extendQuery: (query) => {
+      dispatch(push(Object.assign(ownProps.location.query, query)));
+    },
     dispatch,
   };
 }
