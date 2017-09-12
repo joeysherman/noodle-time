08b700deab60e4b1c352380e077931a8e9ff59d6
Joey Sherman <joey.a.sherman@gmail.com>
2017-09-11 19:35:39 -0700

    Finish extracting AutoComplete

--- app/containers/AutoComplete/actions.js
+++ app/containers/AutoComplete/actions.js
@@ -8,6 +8,38 @@ import {
   DEFAULT_ACTION,
 } from './constants';
 
+/* AutoComplete */
+
+export function autoCompleteRequest(input) {
+  return {
+    type: constants.AUTOCOMPLETE_REQUEST,
+    payload: {
+      input,
+    }
+  }
+}
+
+export function autoCompleteSuccess(response) {
+  return {
+    type: constants.AUTOCOMPLETE_SUCCESS,
+    payload: response,
+  }
+}
+
+export function autoCompleteError(error) {
+  return {
+    type: constants.AUTOCOMPLETE_ERROR,
+    payload: error,
+  }
+}
+
+export function autoCompleteItemSelected(index) {
+  return {
+    type: constants.AUTOCOMPLETE_ITEM_SELECTED,
+    payload: index,
+  }
+}
+
 export function defaultAction() {
   return {
     type: DEFAULT_ACTION,
--- app/containers/HomePage/actions.js
+++ app/containers/HomePage/actions.js
@@ -13,38 +13,6 @@ export function setStatusMessage(message) {
   }
 }
 
-/* AutoComplete */
-
-export function autoCompleteRequest(input) {
-  return {
-    type: constants.AUTOCOMPLETE_REQUEST,
-    payload: {
-      input,
-    }
-  }
-}
-
-export function autoCompleteSuccess(response) {
-  return {
-    type: constants.AUTOCOMPLETE_SUCCESS,
-    payload: response,
-  }
-}
-
-export function autoCompleteError(error) {
-  return {
-    type: constants.AUTOCOMPLETE_ERROR,
-    payload: error,
-  }
-}
-
-export function autoCompleteItemSelected(index) {
-  return {
-    type: constants.AUTOCOMPLETE_ITEM_SELECTED,
-    payload: index,
-  }
-}
-
 export function noodleTime() {
   return {
     type: constants.NOODLE_TIME,
