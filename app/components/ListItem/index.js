a3013355e366379902f51ee472fc16cb5334da94
joeysherman <joeyasherman@gmail.com>
2019-06-09 01:24:55 -0700

    Show review stars

--- app/components/ListItem/index.js
+++ app/components/ListItem/index.js
@@ -18,20 +18,39 @@ function ListItem(props) {
     distance,
   } = props.place;
 
+  let no_star = <i className="material-icons">star_border</i>,
+      half_star = <i className="material-icons">star_half</i>,
+      star = <i className="material-icons">star</i>;
+// mile to meter = 1609.34
+
+  let stars_html = [];
+  let count = 0;
+
+  while (count != 5) {
+    // rating = 0 
+    // rating = 2.5
+    // rating = 5
+    if (rating == 0) {
+      stars_html.push(<p>No Rating</p>);
+    }
+    count += 1; 
+    if (rating >= count) {
+      stars_html.push(star);
+    } else if (count-0.5 == rating) {
+      stars_html.push(half_star);
+    } else {
+      stars_html.push(no_star);
+    }
+  }
+
   return (
-    <li className="collection-item">
-      <div className="row no-pad-bot lighten-2 z-depth-1-half">
-      <a className="btn-floating btn waves-effect waves-light red right-align">{props.index}</a>
-        <div className='col s5 m4 l3'>
-          <div className="section">
-            <img src={image_url} alt="" className="responsive-img"></img>
-          </div>
-        </div>
-        <div className="col s7 m8 l9">
-          <h5>{name}</h5>
-          <p>{rating + ' based off of ' + review_count}</p> 
-        </div>
-      </div>
+    <li className="collection-item avatar">
+      <img src={image_url} alt="" className="circle"></img>
+      <span className="title">{name}</span>
+      <p>{review_count} Reviews.<br></br>
+      {distance}
+      </p>
+      <a href="#!" className="secondary-content">{stars_html}</a>
     </li>
   );
 }
